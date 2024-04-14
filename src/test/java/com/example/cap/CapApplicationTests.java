43b823bdbae931745dc43dc97df5de13826bb868
CaoxOffical <officialCaox@gmail.com>
2024-04-14 23:32:26 +0900

    [Spring Boot] intelliJ

---
+++ src/test/java/com/example/cap/CapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
