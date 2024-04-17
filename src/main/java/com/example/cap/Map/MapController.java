47a97dec6392dc9973428909c6fc0a9162aa6fa6
CaoxOffical <officialCaox@gmail.com>
2024-04-17 23:12:34 +0900

    ✨Feat : MapController, Debug Main log
    MapController 클래스 추가로, 파라미터로 http 키 입력시 json으로 파싱해서 제공하는 기능 추가,
    
    Main함수에 Logging 하는 코드 추가

---
+++ src/main/java/com/example/cap/Map/MapController.java
@@ -0,0 +1,39 @@
+package com.example.cap.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MapController {
+
+    @GetMapping("/allow_info/basic")
+    public static String AllowURL(String URL) {
+        StringBuffer result = new StringBuffer();
+        try {
+            URL url = new URL(URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader rd;
+            if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line + "\n");
+            }
+            rd.close();
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result + "";
+    }
+    }
