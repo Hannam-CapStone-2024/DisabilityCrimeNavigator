b2db1679832ee0b1e96a5dd7ab689b97207152c8
OfficialCaox <125695165+OfficialCaox@users.noreply.github.com>
2024-05-13 04:35:05 +0900

    ✨Feat : Change Project

---
+++ src/main/java/cap/CapApplication.java
@@ -0,0 +1,22 @@
+package cap;
+
+import cap.Map.MapController;
+import cap.controller.CrimeZoneController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapApplication {
+
+    static String key = "https://api.odcloud.kr/api/15014357/v1/uddi:1de84e36-fda0-4396-8de6-a73d000849c9?page=1&perPage=10&serviceKey=1Sn0o0so2SkZMr8X7Rnzhw7jOEf7JcIVseZU%2FFgWkwpdUWIwBPWV9VtC3YLCOqDYa%2F3hCkLoTOxChspnuJRCug%3D%3D";
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CapApplication.class, args);
+
+        System.out.println("API 키: " + MapController.AllowURL(key));
+
+        System.out.println("범죄 : " + CrimeZoneController.GetInstance().GetCrimeZone());
+    }
+
+}
