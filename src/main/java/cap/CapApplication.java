de901eb9f89ba36ea42ed1c1903c2e5a42897d73
OfficialCaox <125695165+OfficialCaox@users.noreply.github.com>
2024-05-13 15:49:26 +0900

    ✨Feat : Setting

--- src/main/java/cap/CapApplication.java
+++ src/main/java/cap/CapApplication.java
@@ -1,7 +1,6 @@
 package cap;
 
 import cap.Map.MapController;
-import cap.controller.CrimeZoneController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
